ff0996340b5147c38703acc4718651e39ec2c89c
Skye Harris-Stoertz <sharrisstoertz@lcs.on.ca>
2025-02-27 09:19:15 -0500

    Initial Commit

---
+++ SpeedCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpeedCalculator
+//
+//  Created by Skye Willow Harris-Stoertz on 2025-02-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpeedCalculator/SpeedCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedCalculatorApp.swift
+//  SpeedCalculator
+//
+//  Created by Skye Willow Harris-Stoertz on 2025-02-27.
+//
+
+import SwiftUI
+
+@main
+struct SpeedCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
